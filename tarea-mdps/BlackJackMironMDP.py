25087480096c4eea32750a44ff68b11c1242498d
Juan Faz <75135385+juancfaz@users.noreply.github.com>
2023-04-20 22:34:48 -0700

    MDP

--- tarea-mdps/BlackJackMironMDP.py
+++ tarea-mdps/BlackJackMironMDP.py
@@ -13,7 +13,7 @@ class BlackJackMDP(MarkovDecisionProcess):
     def startState(self) -> Tuple[int, List[int], List[int]]:
         v = self.v
         v = random.sample(range(1, 14), 13)
-        return (0, [], [self.multiplicity]*len(v))
+        return (0, (), (self.multiplicity,)*len(v))
     
     def actions(self, state: Tuple[int, List[int], List[int]]) -> List[str]:
         total, peek_index, deck_counts = state
@@ -24,7 +24,7 @@ class BlackJackMDP(MarkovDecisionProcess):
         else:
             return ["take", "quit"]
     
-    def transitions(self, state: Tuple[int, List[int], List[int]], action: str) -> List[Tuple[int, List[int], List[int]]]:
+    def transitions(self, state: Tuple[int, Tuple[int], Tuple[int]], action: str) -> List[Tuple[int, Tuple[int], Tuple[int]]]:
         total, peek_index, deck_counts = state
         results = []
         if action == "take":
@@ -32,25 +32,22 @@ class BlackJackMDP(MarkovDecisionProcess):
                 for i in range(len(deck_counts)):
                     if deck_counts[i] > 0:
                         new_total = total + self.v[i]
-                        new_deck_counts = list(deck_counts)
-                        new_deck_counts[i] -= 1
-                        results.append((new_total, [], new_deck_counts))
+                        new_deck_counts = tuple(d - int(i == j) for j, d in enumerate(deck_counts))
+                        results.append((new_total, (), new_deck_counts))
             else:
                 i = peek_index[0]
                 new_total = total + self.v[i]
-                new_deck_counts = list(deck_counts)
-                new_deck_counts[i] -= 1
-                new_peek_index = peek_index[1:]
+                new_deck_counts = tuple(d - int(i == j) for j, d in enumerate(deck_counts))
+                new_peek_index = tuple(peek_index[1:])
                 results.append((new_total, new_peek_index, new_deck_counts))
         elif action == "peek":
             for i in range(len(deck_counts)):
                 if deck_counts[i] > 0:
-                    new_peek_index = peek_index + [i]
-                    new_deck_counts = list(deck_counts)
-                    new_deck_counts[i] -= 1
+                    new_peek_index = tuple(list(peek_index) + [i])
+                    new_deck_counts = tuple(d - int(i == j) for j, d in enumerate(deck_counts))
                     results.append((total, new_peek_index, new_deck_counts))
         elif action == "quit":
-            results.append((total, [], list(deck_counts)))
+            results.append((total, (), tuple(deck_counts)))
         return results
     
     def probability(self, source: Tuple[int, List[int], List[int]], action: str, target: Tuple[int, List[int], List[int]]) -> float:
@@ -81,11 +78,57 @@ class BlackJackMDP(MarkovDecisionProcess):
         else:
             return 0
         
-        
     def isEnd(self, state: Tuple[int, List[int], List[int]]) -> bool:
         total, _, _ = state
         return total > self.threshold or total == 21 or not any(state[2])
 
+
+mdp = BlackJackMDP([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10], 4, 1, 21)
+
+
+'''
+def reachable_states(mdp, n=0):
+    pending = {mdp.startState()}
+    reachable = set()
+    while len(pending) > 0:
+        source = pending.pop()
+        reachable.add(source)
+        for action in mdp.actions(source):
+            for target in mdp.transitions(source, action):
+                if target in reachable:
+                    continue
+                pending.add(target)
+        if n == 100:
+            break
+        n += 1
+    return reachable
+
+def poleval(mdp, policy, n=100):
+    states = reachable_states(mdp)
+    v1 = {s: 0 for s in states}
+    v2 = {}
+
+    def Q(src, act):
+        dis = mdp.discount()
+        return max(sum(p * (r + dis * v1[tgt]) for tgt, p, r in targets(mdp, src, act)))
+
+    for _ in range(n):
+        for s in states:
+            if mdp.isEnd(s):
+                v2[s] = 0
+            else:
+                if policy == "take":
+                    v2[s] = Q(s, "take")
+        v1, v2 = v2, v1
+    return v1
+
+def always_take():
+    return "take"
+
+print(poleval(mdp, always_take))
+'''
+
+'''
 def episodes(mdp):
     ls = list()
     state = mdp.startState()
@@ -100,7 +143,6 @@ def episodes(mdp):
         ls.append([state[0], action, reward, target[0]])
         state=target
     return ls
-
 
 def montecarlo(episodes):
     T = {}
@@ -130,7 +172,7 @@ def montecarlo(episodes):
     return probabilities, R
 
 
-mdp = BlackJackMDP([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10], 4, 1, 21)
+
 eps = []
 for i in range(1000):
     eps.append(episodes(mdp))
@@ -146,3 +188,4 @@ print()
 for state, action in sorted(R.keys()):
     reward = R[(state, action)]
     print("R({}, {}) = {}".format(state, action, reward))
+'''
---
+++ tarea-mdps/driver.py
@@ -0,0 +1,82 @@
+from operator import itemgetter as nth
+from random import random
+from mdp import targets
+from camion import MagicBus
+
+def reachable_states(mdp):
+    pending = {mdp.startState()}
+    print(pending)
+    reachable = set()
+    while len(pending) > 0:
+        source = pending.pop()
+        print(source)
+        reachable.add(source)
+        for action in mdp.actions(source):
+            for target in mdp.transitions(source, action):
+                if target in reachable:
+                    continue
+                pending.add(target)
+    return reachable
+
+def poleval(mdp, policy, n=100):
+    states = reachable_states(mdp)
+    v1 = {s: 0 for s in states}
+    v2 = {}
+
+    def Q(src, act):
+        dis = mdp.discount()
+        return sum(p * (r + dis * v1[tgt]) for tgt, p, r in targets(mdp, src, act))
+
+    for _ in range(n):
+        for s in states:
+            if mdp.isEnd(s):
+                v2[s] = 0
+            else:
+                v2[s] = Q(s, policy(s))
+        v1, v2 = v2, v1
+    return v1
+
+
+def noop(*args):
+    return None
+
+
+def run(mdp, policy, log=print):
+    utility = 0.0
+    state = mdp.startState()
+    k = 0
+    while not mdp.isEnd(state):
+        action = policy(state)
+        accProb = 0.0
+        for target, prob, reward in targets(mdp, state, action):
+            accProb += prob
+            if random() < accProb:
+                log(f"At {state} perform {action} to reach {target}")
+                state = target
+                utility += reward * mdp.discount() ** k
+                break
+        k += 1
+    log(f"Final state {state} reached, returning utility")
+    return utility
+
+
+def experiment(mdp, policy, report, trials=10**4):
+    utilities = {}
+    average = 0.0
+    for _ in range(trials):
+        utility = run(mdp, policy, noop)
+        average += utility
+        utilities[utility] = utilities.get(utility, 0) + 1
+    average /= trials
+    distribution = {utility: count / trials for utility, count in utilities.items()}
+    return report(distribution, average)
+
+
+def plain_report(distribution, average):
+    print("EXPECTED_UTILITY:", average)
+    print("DISTRIBUTION:")
+    print("| utility | probability |")
+    print("|---------+-------------|")
+    for utility, probability in sorted(distribution.items(), key=nth(1), reverse=True):
+        bar = "#" * int(probability * 40)
+        print(f"| {utility:^7.2f} | {probability:^11.5f} |", bar)
